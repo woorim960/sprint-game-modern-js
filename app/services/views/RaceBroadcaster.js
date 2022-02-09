f1f2521c11af9bf17fc25c46161506a140f64f29
박우림 <woorimprog@gmail.com>
2022-02-10 00:51:30 +0900

    Refactor(RaceBroadcaster): 달리기 시작 메세지 출력 메서드 추가

--- app/services/views/RaceBroadcaster.js
+++ app/services/views/RaceBroadcaster.js
@@ -13,6 +13,13 @@ const RaceBroadcaster = {
   sayWinner: function (winner) {
     console.log(`${winner} 선수가 승리하였습니다.`);
   },
+  /**
+   *
+   * @param {number} gameCount
+   */
+  sayStartMessage: function (gameCount) {
+    console.log(`\n:: ${gameCount}번째 달리기 시도 ::`);
+  },
 };
 
 module.exports = RaceBroadcaster;
